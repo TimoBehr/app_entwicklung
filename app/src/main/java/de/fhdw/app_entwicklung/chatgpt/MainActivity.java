6516f7935cc3ab50df89693d4d0af0a7b349d938
Timo Behr <Timo.Behr@gmx.de>
2023-11-02 15:39:42 +0100

    settings api code

--- app/src/main/java/de/fhdw/app_entwicklung/chatgpt/MainActivity.java
+++ app/src/main/java/de/fhdw/app_entwicklung/chatgpt/MainActivity.java
@@ -1,16 +1,40 @@
 package de.fhdw.app_entwicklung.chatgpt;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.TextView;
+
+import de.fhdw.app_entwicklung.chatgpt.openai.PrefsActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView textView;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        startActivity(new Intent(this, PrefsActivity.class));
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/de/fhdw/app_entwicklung/chatgpt/MainFragment.java
+++ app/src/main/java/de/fhdw/app_entwicklung/chatgpt/MainFragment.java
@@ -11,6 +11,7 @@ import androidx.activity.result.ActivityResultLauncher;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.preference.PreferenceManager;
 
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
@@ -21,20 +22,25 @@ import de.fhdw.app_entwicklung.chatgpt.speech.Speak;
 
 public class MainFragment extends Fragment {
 
-    ChatGpt c = new ChatGpt("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");
     Speak speak;
 
     private final ActivityResultLauncher<LaunchSpeechRecognition.SpeechRecognitionArgs> someActivityResultLauncher = registerForActivityResult(
             new LaunchSpeechRecognition(),
             query -> {
-                ((TextView)getView().findViewById(R.id.textView)).append(query);
-                Executor executor = Executors.newFixedThreadPool(1);
-                executor.execute(() -> {
-                    String response = c.getChatCompletion(query);
-                    ((TextView)getView().findViewById(R.id.textView)).append("\n");
-                    ((TextView)getView().findViewById(R.id.textView)).append(response + "\n");
-                    Speak.Ausgabe(response);
-                });
+                if (query != null) {
+                    ChatGpt c = new ChatGpt(PreferenceManager
+                            .getDefaultSharedPreferences(requireContext())
+                            .getString("api_token", ""));
+                    ((TextView)getView().findViewById(R.id.textView)).append(query);
+                    Executor executor = Executors.newFixedThreadPool(1);
+                    executor.execute(() -> {
+                        String response = c.getChatCompletion(query);
+                        ((TextView)getView().findViewById(R.id.textView)).append("\n");
+                        ((TextView)getView().findViewById(R.id.textView)).append(response + "\n");
+                        Speak.Ausgabe(response);
+
+                    });
+                }
             });
 
     public MainFragment() {
