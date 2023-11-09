5426cc178e1560944abbbfe0baf9b9fbded8162c
Timo Behr <Timo.Behr@gmx.de>
2023-11-09 15:34:51 +0100

    Parcelable Chat und Message

--- app/src/main/java/de/fhdw/app_entwicklung/chatgpt/model/Chat.java
+++ app/src/main/java/de/fhdw/app_entwicklung/chatgpt/model/Chat.java
@@ -1,14 +1,35 @@
 package de.fhdw.app_entwicklung.chatgpt.model;
 
+import android.os.Build;
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import androidx.annotation.NonNull;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class Chat {
+public class Chat implements Parcelable {
 
     private final List<Message> messages = new ArrayList<>();
+    public static final Parcelable.Creator<Chat> CREATOR = new Creator<Chat>() {
+        @Override
+        public Chat createFromParcel(Parcel source) {
+            return new Chat(source);
+        }
+
+        @Override
+        public Chat[] newArray(int size) {
+            return new Chat[size];
+        }
+    };
 
     public Chat() {
+    }
+
+    public Chat(Parcel source) {
+       source.readList(this.messages, Message.class.getClassLoader());
     }
 
     public void addMessage(Message message) {
@@ -17,5 +38,15 @@ public class Chat {
 
     public List<Message> getMessages() {
         return Collections.unmodifiableList(messages);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(@NonNull Parcel dest, int flags) {
+            dest.writeList(messages);
     }
 }
--- app/src/main/java/de/fhdw/app_entwicklung/chatgpt/model/Message.java
+++ app/src/main/java/de/fhdw/app_entwicklung/chatgpt/model/Message.java
@@ -1,8 +1,13 @@
 package de.fhdw.app_entwicklung.chatgpt.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import androidx.annotation.NonNull;
+
 import java.util.Date;
 
-public class Message {
+public class Message implements Parcelable {
     public final Date date;
     public final Author author;
     public final String message;
@@ -11,9 +16,39 @@ public class Message {
         this(new Date(), author, message);
     }
 
+    public static final Parcelable.Creator<Message> CREATOR = new Creator<Message>() {
+        @Override
+        public Message createFromParcel(Parcel source) {
+            return new Message(source);
+        }
+
+        @Override
+        public Message[] newArray(int size) {
+            return new Message[size];
+        }
+    };
+
     public Message(Date date, Author author, String message) {
         this.date = date;
         this.author = author;
         this.message = message;
+    }
+
+    public Message(Parcel source) {
+        this.date = new Date(source.readLong());
+        this.author = Author.valueOf(source.readString());
+        this.message = source.readString();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(@NonNull Parcel dest, int flags) {
+        dest.writeLong(date.getTime());
+        dest.writeString(author.name());
+        dest.writeString(message);
     }
 }
